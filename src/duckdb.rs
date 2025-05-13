50acdc82f54402b2e7719164cf81ec7df7e1d5c6
Luke Sneeringer <luke@sneeringer.com>
2025-05-12 19:22:02 -0700

    fix: Also support duckdb `Timestamp` type.

--- src/duckdb.rs
+++ src/duckdb.rs
@@ -12,31 +12,41 @@ use duckdb::types::ValueRef;
 use crate::DateTime;
 use crate::Precision;
 
-impl FromSql for DateTime {
-  fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
-    match value {
-      ValueRef::Time64(TimeUnit::Second, seconds) => Ok(DateTime::from_timestamp(seconds, 0)),
-      ValueRef::Time64(TimeUnit::Millisecond, millis) =>
-        Ok(DateTime::from_timestamp(millis / 1_000, (millis % 1_000) as u32 * 1_000_000)),
-      ValueRef::Time64(TimeUnit::Microsecond, micros) =>
-        Ok(DateTime::from_timestamp(micros / 1_000_000, (micros % 1_000_000) as u32 * 1_000)),
-      ValueRef::Time64(TimeUnit::Nanosecond, nanos) =>
-        Ok(DateTime::from_timestamp(nanos / 1_000_000_000, (nanos % 1_000_000_000) as u32)),
-      _ => Err(FromSqlError::InvalidType),
+macro_rules! from_sql {
+  ($($precision:ident($var:ident) => $e:expr)*) => {
+    impl FromSql for DateTime {
+      fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        match value {
+          $(ValueRef::Timestamp(TimeUnit::$precision, $var) => Ok($e),
+          ValueRef::Time64(TimeUnit::$precision, $var) => Ok($e),)*
+          _ => Err(FromSqlError::InvalidType),
+        }
+      }
     }
   }
+}
+
+from_sql! {
+  Second(seconds) => DateTime::from_timestamp(seconds, 0)
+  Millisecond(ms) => DateTime::from_timestamp(ms / 1_000, (ms % 1_000) as u32 * 1_000_000)
+  Microsecond(us) => DateTime::from_timestamp(us / 1_000_000, (us % 1_000_000) as u32 * 1_000)
+  Nanosecond(ns)  => DateTime::from_timestamp(ns / 1_000_000_000, (ns % 1_000_000_000) as u32)
 }
 
 impl ToSql for DateTime {
   fn to_sql(&self) -> Result<ToSqlOutput<'_>> {
     match self.precision() {
       Precision::Second =>
-        Ok(ToSqlOutput::Borrowed(ValueRef::Time64(TimeUnit::Second, self.as_seconds()))),
-      Precision::Millisecond =>
-        Ok(ToSqlOutput::Borrowed(ValueRef::Time64(TimeUnit::Millisecond, self.as_milliseconds()))),
-      Precision::Microsecond =>
-        Ok(ToSqlOutput::Borrowed(ValueRef::Time64(TimeUnit::Microsecond, self.as_microseconds()))),
-      Precision::Nanosecond => Ok(ToSqlOutput::Borrowed(ValueRef::Time64(
+        Ok(ToSqlOutput::Borrowed(ValueRef::Timestamp(TimeUnit::Second, self.as_seconds()))),
+      Precision::Millisecond => Ok(ToSqlOutput::Borrowed(ValueRef::Timestamp(
+        TimeUnit::Millisecond,
+        self.as_milliseconds(),
+      ))),
+      Precision::Microsecond => Ok(ToSqlOutput::Borrowed(ValueRef::Timestamp(
+        TimeUnit::Microsecond,
+        self.as_microseconds(),
+      ))),
+      Precision::Nanosecond => Ok(ToSqlOutput::Borrowed(ValueRef::Timestamp(
         TimeUnit::Nanosecond,
         (self.as_nanoseconds().try_into())
           .map_err(|e| duckdb::Error::ToSqlConversionFailure(Box::new(e)))?,
@@ -58,6 +68,9 @@ mod tests {
     for (precision, multiplier) in
       [(Second, 1), (Millisecond, 1_000), (Microsecond, 1_000_000), (Nanosecond, 1_000_000_000)]
     {
+      let input = ValueRef::Timestamp(precision, 1335020400 * multiplier);
+      let dt = DateTime::column_result(input)?;
+      check!(dt == datetime! { 2012-04-21 15:00:00 });
       let input = ValueRef::Time64(precision, 1335020400 * multiplier);
       let dt = DateTime::column_result(input)?;
       check!(dt == datetime! { 2012-04-21 15:00:00 });
@@ -69,7 +82,7 @@ mod tests {
   fn test_to_sql() -> Result<()> {
     let dt = datetime! { 2012-04-21 15:00:00 };
     let output = dt.to_sql()?;
-    if let ToSqlOutput::Borrowed(ValueRef::Time64(TimeUnit::Second, seconds)) = output {
+    if let ToSqlOutput::Borrowed(ValueRef::Timestamp(TimeUnit::Second, seconds)) = output {
       check!(seconds == 1335020400);
     } else {
       check!(false, "Incorrect type");
